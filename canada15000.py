d11e4f1dba7f5ec30419a3008f838cefda7a369c
johnchabot <75501303+johnchabot@users.noreply.github.com>
2026-08-09 15:10:33 -0400

    Update canada15000.py

--- canada15000.py
+++ canada15000.py
@@ -205,17 +205,14 @@ def generate_sprite_sheet(font_path):
   
     glyphs_data = {}
 
-    
+        # === SURGICAL SAFETNET PATCH ===
 
-
-
-    # === SURGICAL SAFETNET PATCH ===
     for char, code in CHARACTER_SET.items():
-        try:
-            res = extract_glyph_with_bounds(font, unicode_map, char, BASE_CELL_W, BASE_CELL_H)
-            if res is None:
-                continue
-            path, transform, _, _, glyph_name = res
+        res = extract_glyph_with_bounds(font, unicode_map, char, BASE_CELL_W, BASE_CELL_H)
+        if res is None: continue
+        path, transform, _, _, glyph_name = res
+
+
         except TypeError:
             # Safely catch any unpack error if a rogue None leaks out
             continue
